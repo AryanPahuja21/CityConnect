31bf63e026ff0ea06641c55996d19e7c77ab174c
rohitk131 <rohitkumar13103@gmail.com>
2024-02-09 14:48:26 +0530

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Components/Navbar'; 
+import HeroSection from './Components/heroSection'; 
+function App() {
+  
+  return (
+    <>
+    <div className=''>
+      <Navbar />
+      <HeroSection />
+    </div>
+    </>
+  )
+}
+
+export default App
